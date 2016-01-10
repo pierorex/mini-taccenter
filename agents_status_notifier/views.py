afa9b0edc0bcc68b4d16f83dfba0508696ed0df8
Pierorex <jeanpierohm@gmail.com>
2016-01-10 06:00:27 +0000

    Added POST /agentstatus so agents can be changed by a supervisor

--- agents_status_notifier/views.py
+++ agents_status_notifier/views.py
@@ -19,11 +19,24 @@ class AgentStatusView(APIView):
         
         return Response(data=status_dict, status=status.HTTP_200_OK)
 
+    def post(self, request, *args, **kwargs):
+        # Check basic conditions and return HTTP statuses accordingly
+        agent_status = request.data.get('status', None)
+        agent_ext = request.data.get('agent_ext', None)
+        agent_name = request.data.get('agent', None)
+        
+        if not (agent_ext and agent_status and agent_name and
+                status in ['paused', 'available', 'in-call', 'ringing',
+                           'categorizing', 'pause_request']):
+            return Response(data={'error': 'Missing something.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        return Response(data={}, status=status.HTTP_200_OK)
+
 
 class AgentView(APIView):
     
     def get(self, query_dict):
-        username = query_dict.GET['username']
-        return Response(data={'username': username, 
-                               'tmp_ext': random.randint(1,1000)},
+        # Return some mocked random information
+        return Response(data={'username': query_dict.GET['username'], 
+                              'tmp_ext': random.randint(1,1000)},
                         status=status.HTTP_200_OK)
